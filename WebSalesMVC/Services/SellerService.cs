997d26675349dbfbe85191378aa8ed2bb317f5ac
Leonardo Lima <leonardolimaf54@gmail.com>
2021-07-24 15:02:33 -0300

    SellerService and basic FindAll

---
+++ WebSalesMVC/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebSalesMVC.Data;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Services
+{
+    public class SellerService
+    {
+        private readonly WebSalesMVCContext _context;
+
+        public SellerService(WebSalesMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
