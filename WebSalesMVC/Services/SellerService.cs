363fb9ab877ddf8f0b058cce7b8b921c77c59523
Leonardo Lima <leonardolimaf54@gmail.com>
2021-08-01 16:23:37 -0300

    SellerFormViewModel and Department select component

--- WebSalesMVC/Services/SellerService.cs
+++ WebSalesMVC/Services/SellerService.cs
@@ -21,7 +21,6 @@ namespace WebSalesMVC.Services
 
         public void Insert(Seller seller)
         {
-            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
