584b000695a2fc0c0785b8cedb8af8229bddf768
Leonardo Lima <leolimaf@outlook.com>
2021-09-07 12:02:30 -0300

    Implementing simple search

---
+++ WebSalesMVC/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesMVC.Data;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly WebSalesMVCContext _context;
+
+        public SalesRecordService(WebSalesMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = _context.SalesRecord.Select(obj => obj);
+            if (Equals(minDate.HasValue))
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
